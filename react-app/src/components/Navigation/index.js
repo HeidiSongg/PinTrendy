d736bfdb296d644c90b2270642728696db3a189c
heidisong0814 <heidisong0814@gmail.com>
2023-05-14 13:49:12 -0700

    replaced logo

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
-import img from './pinterest.jpeg'
+import img from './logo.png'
 import SearchBar from '../SearchBar';
 
 function Navigation({ isLoaded }) {
